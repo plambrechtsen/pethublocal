2c418424501b7871de2326471f1002e55986eeae
Peter Lambrechtsen <peter@crypt.nz>
2021-03-12 23:31:46 +1300

    Initial Commit

---
+++ WemosPetHub/include/main.h
@@ -0,0 +1,16 @@
+//Static configuration parameters.
+
+//WiFi settings
+#define WIFI_SSID "..."
+#define WIFI_PASSWORD "..."
+
+//MQTT Settings
+#define MQTT_SERVER "192.168.20.241"
+#define MQTT_CLIENT "SurePetTest"
+
+#define DEBUG false
+#define DEBUGMQTT false
+#define DEBUGMESSAGES false
+#define DEBUGSTATE false
+#define DEBUGPACKET false
+#define DEBUGLENGTH false
