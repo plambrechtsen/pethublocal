6007a737cca224546b6532f5f3056f1f03be23ac
Peter Lambrechtsen <peter@crypt.nz>
2021-03-27 02:34:31 +1300

    Entrypoint typo

--- docker/local/msgs/docker-entrypoint.sh
+++ docker/local/msgs/docker-entrypoint.sh
@@ -5,4 +5,5 @@ mkdir -p /mqtt/msgs
 
 cd /mqtt
 
-exec /usr/bin/mosquitto_sub -h local_mqtt -p 1883 -v -t "pethublocal/#" | ts | tee /mqtt/msgs/mqtt."$(date +%Y-%m-%d)".log | python /mqtt/mqttsub.py $(date +%Y-%m-%d)
+exec /usr/bin/mosquitto_sub -h local_mqtt -p 1883 -v -t "pethublocal/#" | ts | tee /mqtt/msgs/mqtt."$(date +%Y-%m-%d)".log 
+#| python /mqtt/mqttsub.py $(date +%Y-%m-%d)
