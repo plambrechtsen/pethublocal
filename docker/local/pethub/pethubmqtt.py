f06203667031f9c222284e9a7d9e2d2b6f241f98
Peter Lambrechtsen <peter@crypt.nz>
2021-04-02 22:18:39 +1300

    Updates to create automatic mqtt discovery in home assistant.

--- docker/local/pethub/pethubmqtt.py
+++ docker/local/pethub/pethubmqtt.py
@@ -1,5 +1,5 @@
 #import logging
-import asyncio, json
+import asyncio, json, ast
 import pethubpacket as phlp
 from enum import IntEnum
 
@@ -37,6 +37,19 @@ class AnimalState(SureEnum): # Animal State
     Outside          = 0
     Inside           = 1
 
+class LockState(SureEnum): # Lock State IDs.
+    UNLOCKED        = 0
+    LOCKED_IN       = 1
+    LOCKED_OUT      = 2
+    LOCKED_ALL      = 3
+    CURFEW          = 4
+
+class CurfewState(SureEnum): # Sure Petcare API State IDs.
+    DISABLED        = 0
+    ENABLED         = 1
+    STATE2          = 2
+    STATE3          = 3
+
 async def hubtolocal(hub_url, local_url, loop):
     print("Pethublocal MQTT HubToLocal Listner starting")
     hub = MQTTClient(loop=loop, config={'keep_alive': 60})
@@ -51,22 +64,40 @@ async def hubtolocal(hub_url, local_url, loop):
         for pet in pethubinit['pets']:
             print(pet)
             if pet[3] == 3: #Pet Door
-                #configmessage='{"name": "{0} inside", "unique_id": "pet_{1}_inside", "icon": "mdi:{2}", "state_topic": "homeassistant/sensor/pethub/pet_{1}_fed1/state"}'.format(pet[0],pet[0].lower(),Animal(pet[1]))
-                #configmessage={"name": pet[0]+" inside", "device_class": "occupancy", "unique_id": "pet_"+pet[0].lower(), "state_topic": mqtt_local_t + "sensor/pethub/pet_"+pet[0].lower()+"/state"}
                 configmessage={"name": pet[0], "icon": "mdi:"+Animal(pet[1]).name, "unique_id": "pet_"+pet[0].lower(), "state_topic": mqtt_local_t + "sensor/pethub/pet_"+pet[0].lower()+"/state"}
-                print(configmessage)
                 asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'/config', message=json.dumps(configmessage).encode('utf-8'), qos=QOS_0))
                 asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'/state', message=AnimalState(pet[4]).name.encode('utf-8'), qos=QOS_0))
-                #asyncio.create_task(local.publish(topic=localtopic + 'action', message=feedermsg['FA'].encode('utf-8') , qos=QOS_0))
-                #print(pet[3])
-                #sensor/pethub/pet_Wicket_fed1/config'
-                #asyncio.create_task(local.publish(topic=localtopic + 'action', message=feedermsg['FA'].encode('utf-8') , qos=QOS_0))
-#            if pet[3] == 4: #Feeder
-                #configmessage='{"name": "{0} inside", "unique_id": "pet_{1}_inside", "icon": "mdi:{2}", "state_topic": "homeassistant/sensor/pethub/pet_{1}_fed1/state"}'.format(pet[0],pet[0].lower(),Animal(pet[1]))
-#                configmessage={"name": pet[0]+" feed", "unique_id": "pet_"+pet[0].lower(), "icon": "mdi:{2}", "state_topic": mqtt_local_t + "binary_sensor/pethub/pet_"+pet[0].lower()+"/state"}
-#                print(configmessage)
-#                asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'_feed/config', message=json.dumps(configmessage).encode('utf-8'), qos=QOS_0))
-#                asyncio.create_task(local.publish(topic=mqtt_local_t + 'binary_sensor/pethub/pet_'+pet[0].lower()+'/state', message=AnimalState(pet[4]).name.encode('utf-8'), qos=QOS_0))
+            if pet[3] == 4: #Feeder
+                feederarray = ast.literal_eval(pet[4])
+                if len(feederarray)==2:
+                    bowlstate = {"left":str(feederarray[0]),"right":str(feederarray[1])}
+                    print(bowlstate)
+                    leftconfigmessage={"name": pet[0]+" left", "icon": "mdi:"+Animal(pet[1]).name, "unique_id": "pet_"+pet[0].lower()+"_left", "state_topic": mqtt_local_t + "sensor/pethub/pet_"+pet[0].lower()+"_bowl/state", "unit_of_measurement": "g", "value_template": "{{value_json.left}}"}
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'_left/config', message=json.dumps(leftconfigmessage).encode('utf-8'), qos=QOS_0))
+                    rightconfigmessage={"name": pet[0]+" right", "icon": "mdi:"+Animal(pet[1]).name, "unique_id": "pet_"+pet[0].lower()+"_right", "state_topic": mqtt_local_t + "sensor/pethub/pet_"+pet[0].lower()+"_bowl/state", "unit_of_measurement": "g", "value_template": "{{value_json.right}}"}
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'_right/config', message=json.dumps(rightconfigmessage).encode('utf-8'), qos=QOS_0))
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'_bowl/state', message=json.dumps(bowlstate).encode('utf-8'), qos=QOS_0))
+                else:
+                    bowlconfigmessage={"name": pet[0]+" bowl", "icon": "mdi:"+Animal(pet[1]).name, "unique_id": "pet_"+pet[0].lower()+"_bowl", "state_topic": mqtt_local_t + "binary_sensor/pethub/pet_"+pet[0].lower()+"_bowl/state"}
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'_bowl/config', message=json.dumps(bowlconfigmessage).encode('utf-8'), qos=QOS_0))
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/pet_'+pet[0].lower()+'_bowl/state', message=pet[4].encode('utf-8'), qos=QOS_0))
+        for device in pethubinit['devices']:
+            print(device)
+            if device[1] == 3: #Pet Door
+                print(device[7])
+                configmessage={"name": device[0], "icon": "mdi:door", "unique_id": "device_"+device[0].lower(), "state_topic": mqtt_local_t + "sensor/pethub/device_"+device[0].lower()+"/state"}
+                asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/device_'+device[0].lower()+'/config', message=json.dumps(configmessage).encode('utf-8'), qos=QOS_0))
+                asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/device_'+device[0].lower()+'/state', message=LockState(device[7]).name.encode('utf-8'), qos=QOS_0))
+                if device[4] != "None" and device[5] != "None" and device[6] != "None":
+                    curfewstate = {"state": CurfewState(device[4]).name, "lock":str(device[5]),"unlock":str(device[6])}
+                    print(curfewstate)
+                    configmessage={"name": device[0]+" curfew", "icon": "mdi:door", "unique_id": "device_"+device[0].lower()+"_curfew", "state_topic": mqtt_local_t + "sensor/pethub/device_"+device[0].lower()+"_curfew/state", "value_template": "{{value_json.state}}"}
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/device_'+device[0].lower()+'_curfew/config', message=json.dumps(configmessage).encode('utf-8'), qos=QOS_0))
+                    configmessage={"name": device[0]+" lock time", "icon": "mdi:door", "unique_id": "device_"+device[0].lower()+"_lock_time", "state_topic": mqtt_local_t + "sensor/pethub/device_"+device[0].lower()+"_curfew/state", "value_template": "{{value_json.lock}}"}
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/device_'+device[0].lower()+'_lock/config', message=json.dumps(configmessage).encode('utf-8'), qos=QOS_0))
+                    configmessage={"name": device[0]+" unlock time", "icon": "mdi:door", "unique_id": "device_"+device[0].lower()+"_unlock_time", "state_topic": mqtt_local_t + "sensor/pethub/device_"+device[0].lower()+"_curfew/state", "value_template": "{{value_json.unlock}}"}
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/device_'+device[0].lower()+'_unlock/config', message=json.dumps(configmessage).encode('utf-8'), qos=QOS_0))
+                    asyncio.create_task(local.publish(topic=mqtt_local_t + 'sensor/pethub/device_'+device[0].lower()+'_curfew/state', message=json.dumps(curfewstate).encode('utf-8'), qos=QOS_0))
                 
         while True:
             if processingmessage == False:
